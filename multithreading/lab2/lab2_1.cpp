0af33dae9c21f2c8e07245a3d12af74ccc4007eb
Ranil Sadykov <Ranil2086@bk.ru>
2024-09-29 19:31:44 +0500

    lab2

--- multithreading/lab2/lab2_1.cpp
+++ multithreading/lab2/lab2_1.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 #include <omp.h>
 
+<<<<<<< HEAD
 long long num_steps = 1000000000;
 double step;
 
@@ -35,3 +36,54 @@ int main(int argc, char* argv[])
 	printf("The value of PI is %15.12f\n", pi);
 	printf("The time to calculate PI was %ld seconds\n", stop - start);
 }
+=======
+using namespace std;
+
+static long num_steps = 10000;
+double step;
+
+int main() {
+  // --------------Sequential--------------
+  {
+    int i;
+    double x, pi, sum = 0.0;
+    double start_time = omp_get_wtime();
+
+    step = 1.0 / (double) num_steps;
+
+    for (i = 0; i < num_steps; i++) {
+      x = (i + 0.5) * step;
+      sum = sum + 4.0 / (1.0 + x * x);
+    }
+
+    pi = step * sum;
+    double end_time = omp_get_wtime();
+
+    printf("Calculated pi = %f\n", pi);
+    printf("Sequential execution time: %f seconds\n", end_time - start_time);
+  }
+
+  // --------------Multi-threaded--------------
+  {
+    int i;
+    double x, pi, sum = 0.0;
+    double start_time = omp_get_wtime();
+
+    step = 1.0 / (double) num_steps;
+
+    // reduction(+:sum) creates local copies of the sum variable for each thread and also sums the values when parallel region is ends
+    #pragma omp parallel for private(x) reduction(+:sum)
+    for (i = 0; i < num_steps; i++) {
+      x = (i + 0.5) * step;
+      sum += 4.0 / (1.0 + x * x);
+    }
+
+    pi = step * sum;
+    double end_time = omp_get_wtime();
+
+    printf("Calculated pi = %f\n", pi);
+    printf("Parallel execution with reduction took: %f seconds\n", end_time - start_time);
+
+  }
+}
+>>>>>>> 995bc7f (lab2)
