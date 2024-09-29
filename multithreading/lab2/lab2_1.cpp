4ee5634dc0d3e5bb81ad5d0163444f9234a1043e
Ranil Sadykov <Ranil2086@bk.ru>
2024-09-29 20:14:27 +0500

    fix

--- multithreading/lab2/lab2_1.cpp
+++ multithreading/lab2/lab2_1.cpp
@@ -1,52 +1,37 @@
 #include <bits/stdc++.h>
 #include <omp.h>
 
-using namespace std;
-
-static long num_steps = 10000;
+long long num_steps = 1000000000;
 double step;
 
-int main() {
-  // --------------Sequential--------------
-  {
-    int i;
-    double x, pi, sum = 0.0;
-    double start_time = omp_get_wtime();
+int main(int argc, char* argv[])
+{
+	clock_t start, stop;
+	double x, pi, sum = 0.0;
+	int i;
 
-    step = 1.0 / (double) num_steps;
+	step = 1.0 / (double)num_steps;
+	start =  omp_get_wtime();
 
-    for (i = 0; i < num_steps; i++) {
-      x = (i + 0.5) * step;
-      sum = sum + 4.0 / (1.0 + x * x);
-    }
+	#pragma omp parallel
+	{
+		double local_sum = 0.0;
+		#pragma omp for
+		for (i = 0; i < num_steps; i++)
+		{
+			x = (i + 0.5) * step;
+			local_sum += 4.0 / (1.0 + x * x);
+		}
+		
+		#pragma omp critical
+		{
+			sum += local_sum;
+		}
+	}
 
-    pi = step * sum;
-    double end_time = omp_get_wtime();
+	pi = sum * step;
+	stop =  omp_get_wtime();
 
-    printf("Calculated pi = %f\n", pi);
-    printf("Sequential execution time: %f seconds\n", end_time - start_time);
-  }
-
-  // --------------Multi-threaded--------------
-  {
-    int i;
-    double x, pi, sum = 0.0;
-    double start_time = omp_get_wtime();
-
-    step = 1.0 / (double) num_steps;
-
-    // reduction(+:sum) creates local copies of the sum variable for each thread and also sums the values when parallel region is ends
-    #pragma omp parallel for private(x) reduction(+:sum)
-    for (i = 0; i < num_steps; i++) {
-      x = (i + 0.5) * step;
-      sum += 4.0 / (1.0 + x * x);
-    }
-
-    pi = step * sum;
-    double end_time = omp_get_wtime();
-
-    printf("Calculated pi = %f\n", pi);
-    printf("Parallel execution with reduction took: %f seconds\n", end_time - start_time);
-
-  }
+	printf("The value of PI is %15.12f\n", pi);
+	printf("The time to calculate PI was %ld seconds\n", stop - start);
 }
